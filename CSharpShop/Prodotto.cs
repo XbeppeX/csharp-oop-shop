7104d34b15b4e2cfc60914a8eeedcd21cce2bee8
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-08 15:15:44 +0200

    prezzo

--- CSharpShop/Prodotto.cs
+++ CSharpShop/Prodotto.cs
@@ -3,8 +3,46 @@ namespace CSharpShop
 {
 	public class Prodotto
 	{
-		public Prodotto()
+		private int codice;
+		public string nome;
+		public string descrizione;
+		public int prezzo;
+		public int iva;
+
+		public Prodotto(string nome1 , string descrizione1, int prezzo1, int iva1 )
 		{
+			nome = nome1;
+			descrizione = descrizione1;
+			prezzo = prezzo1;
+			iva = iva1;
+
+		}
+
+		public double CalcoloPrezzoConIva()
+        {
+			double ivato = prezzo * iva / 100;
+			double prezzoFinale = prezzo + ivato;
+			return prezzoFinale;
+        }
+
+		public int NumeroCasualeCodice()
+        {
+			Random r = new Random();
+			return r.Next(100000000);
+        }
+		public void StampaPrezzoSenzaIva()
+        {
+			Console.WriteLine("Prezzo prodotto senza Iva: " + prezzo + "€");
+        }
+
+		public void StampaPrezzoConIva()
+		{
+			Console.WriteLine("Prezzo prodotto con Iva: " + CalcoloPrezzoConIva() + " €");
+		}
+
+		public void StampaNomeCodice()
+		{
+			Console.WriteLine("Nome esteso prodotto " + NumeroCasualeCodice() + nome);
 		}
 	}
 }
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CSharpShop;
+
+
+
+Prodotto Mioprodotto = new Prodotto("banane", "Provenienza Africa", 3, 4);
+
+Mioprodotto.StampaPrezzoConIva();
+
+Mioprodotto.StampaPrezzoSenzaIva();
+
+Mioprodotto.StampaNomeCodice();
 
